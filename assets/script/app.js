22deffa3ffdd02fade936410a07610937b87eb07
Navjot Kaur <saininavjot357019@gmail.com>
2024-04-11 13:22:34 -0500

    Completed functions

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,48 +1,38 @@
 'use strict';
 
-function listen(event, selector, callback) {
-    return selector.addEventListener(event, callback);
-}
-   
-function select(selector) {
-    return document.querySelector(selector);
-}
-  
-const trackButton = select('.primary-icon-btn');
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VybGlua2F1ciIsImEiOiJjbHExYjM4cHUwNzE3MnBud25qNDlmc2VjIn0.Jeu9BD0h1vILAwXce8dQqw';
 
-function getLocation(position) {
-    let { latitude, longitude } = position.coords;
-  
-    setupMap([longitude, latitude], longitude, latitude);
-}
-
 const options = {
-    enableHighAccuracy: true
+  enableHighAccuracy: true
 };
 
-function errorHandler() {
-    return 'Unable to retrieve your location';
-}
-
 function setupMap(center, longitude, latitude) {
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
-        zoom: 15
-    });
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: center,
+    zoom: 15
+  });
 
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav);
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
 
-    const marker1 = new mapboxgl.Marker({color: '#ff7342'})
-    .setLngLat([longitude, latitude])
-    .addTo(map);
+  const marker1 = new mapboxgl.Marker({color: '#ff7342'})
+  .setLngLat([longitude, latitude])
+  .addTo(map);
 }
 
-function validate() {
+function getLocation(position) {
+  let { latitude, longitude } = position.coords;
+
+  setupMap([longitude, latitude], longitude, latitude);
+}
+
+function errorHandler() {
+  return 'Unable to retrieve your location';
+}
+
+function displayPosition() {
   if('geolocation' in navigator) {
       navigator.geolocation.watchPosition(getLocation, errorHandler, options);
   } else {
@@ -50,4 +40,4 @@ function validate() {
   }
 }
 
-validate();
+displayPosition();
